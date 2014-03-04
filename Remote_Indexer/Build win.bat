ba5675d268541908d780fc86ee0cba665ab21153
Chris Knowles <s_razer@yahoo.com>
2014-03-04 12:20:58 -0500

    Add 32bit executable.
    -Updated build script to build win32 & win64 executables.
    -Added 32bit executable.

--- Remote_Indexer/Build win.bat
+++ Remote_Indexer/Build win.bat
@@ -1,13 +1,22 @@
 @echo off
 
 echo #################################################################
-echo Build Windows Executable
+echo Build Windows Executables
 echo Note: Requires PyInstaller
 echo #################################################################
 
-pyinstaller -F RemIdx.py
+echo Building 64bit
+pyinstaller -F RemIdx.py -n RemIdx-64bit
 echo Moving dist\RemIdx.exe to .
-move /y dist\RemIdx.exe .
+move /y dist\RemIdx-64bit.exe .
+
+rmdir /s/q dist
+rmdir /s/q build
+
+echo Building 32bit
+C:\Python27-32bit\Scripts\pyinstaller -F RemIdx.py -n RemIdx-32bit
+echo Moving dist\RemIdx-32bit.exe to .
+move /y dist\RemIdx-32bit.exe .
 
 rmdir /s/q dist
 rmdir /s/q build
